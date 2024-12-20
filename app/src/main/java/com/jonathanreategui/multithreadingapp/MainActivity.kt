06dfa234c78186d6690de5ef8bf637b4723c798b
Jonathan Reátegui <j.reategui2012@gmail.com>
2024-12-20 01:54:54 -0500

    Launching demo threads on click button

--- app/src/main/java/com/jonathanreategui/multithreadingapp/MainActivity.kt
+++ app/src/main/java/com/jonathanreategui/multithreadingapp/MainActivity.kt
@@ -1,20 +1,60 @@
 package com.jonathanreategui.multithreadingapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvThread1: TextView
+    private lateinit var tvThread2: TextView
+    private lateinit var tvThread3: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        tvThread1 = findViewById(R.id.tvThread1)
+        tvThread2 = findViewById(R.id.tvThread2)
+        tvThread3 = findViewById(R.id.tvThread3)
+    }
+
+    fun buttonClicked(view: View) {
+        launchThread1()
+        launchThread2()
+        launchThread3()
+    }
+
+    private fun launchThread1() {
+        val runnable1 = Runnable {
+            runOnUiThread {
+                tvThread1.text = "First thread active."
+            }
         }
+        val thread1 = Thread(runnable1)
+        thread1.start()
+    }
+
+    private fun launchThread2() {
+        val runnable2 = Runnable {
+            Handler(Looper.getMainLooper()).postDelayed({
+                tvThread2.text = "Second thread active."
+            }, 1500)
+        }
+        val thread2 = Thread(runnable2)
+        thread2.start()
+    }
+
+    private fun launchThread3() {
+        val runnable3 = Runnable {
+            Handler(Looper.getMainLooper()).postDelayed({
+                tvThread3.text = "Third thread active."
+            }, 3000)
+        }
+        val thread3 = Thread(runnable3)
+        thread3.start()
     }
 }
+
